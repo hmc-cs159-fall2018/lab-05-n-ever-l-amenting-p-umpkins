438deededcc95faf88c612af011ef03320256109
celena <celenachen7@gmail.com>
2018-10-07 20:26:11 -0700

    added bigram, unigram, and cm score functions

--- SpellCheck.py
+++ SpellCheck.py
@@ -10,13 +10,31 @@ class SpellChecker():
     def load_language_model(fp): 
 
     def bigram_score(prev_word, focus_word, next_word): 
-    	return 
+        """
+        takes 3 words and returns the average bigram probability of the first and last pair
+        """
+        return (self.language_model.bigram_prob(prev_word, focus_word) + self.language_model.bigram_prob(focus_word, next_word)) / 2
 
     def unigram_score(word): 
-    	return 
+        """
+        takes a word and returns the unigram probability
+        """
+    	return self.language_model.unigram_prob(word)
 
     def cm_score(error_word, corrected_word): 
-    	return
+        """
+        gives the probability of a word having been transformed into a given erroneous form
+
+        params
+        ------
+        error_word     - the observed misspelling
+        corrected_word - the proposed corrected word
+
+        returns
+        -------
+        prob           - the probability of the corrected word having been transformed into the error word
+        """
+    	return self.channel_model.prob(error_word, corrected_word)
 
     def inserts(word):
     	return 
